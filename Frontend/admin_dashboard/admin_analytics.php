4e791198e958345d2ca8c2f3436e07e800276917
GRIMAGES <redmercy.ros@gmail.com>
2025-03-14 06:46:32 +0800

    updated

--- Frontend/admin_dashboard/admin_analytics.php
+++ Frontend/admin_dashboard/admin_analytics.php
@@ -76,7 +76,7 @@ $colors = array_map(function($item) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Performance Analytics</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/admin css/dashboard.css">
+    <link rel="stylesheet" href="../CSS/admin_css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- Include Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
