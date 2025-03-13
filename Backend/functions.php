cbedf03634e2121e34eefce1be8db531733a3946
GRIMAGES <redmercy.ros@gmail.com>
2025-03-13 19:51:51 +0800

    updated

--- Backend/functions.php
+++ Backend/functions.php
@@ -32,7 +32,7 @@ function sendOTPByEmail($toEmail, $username, $otp, $subject) {
         $mail->addAddress($toEmail, $username);
 
         // Generate the verification link
-        $verificationLink = "https://kaluppa.online/Backend/otpverification.php?email=" . urlencode($toEmail) . "&otp=" . urlencode($otp);
+        $verificationLink = "https://kaluppa.online/Kaluppa/Backend/otpverification.php?email=" . urlencode($toEmail) . "&otp=" . urlencode($otp);
 
 
         // Set email content with the verification link
