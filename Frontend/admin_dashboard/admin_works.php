72a8eef8f6321ec0a8ae6438327ce86ac2576550
GRIMAGES <redmercy.ros@gmail.com>
2025-03-15 02:34:18 +0800

    update

--- Frontend/admin_dashboard/admin_works.php
+++ Frontend/admin_dashboard/admin_works.php
@@ -250,20 +250,6 @@ if (isset($_GET['id'])) {
     <i class="fas fa-plus"></i> Add Work
 </button>
 
-<?php if (isset($_SESSION['toast_success'])): ?>
-    <div class="position-fixed top-0 end-0 p-3" style="z-index: 1055;">
-        <div class="toast align-items-center text-white bg-success border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="d-flex">
-                <div class="toast-body">
-                    <?php echo $_SESSION['toast_success']; ?>
-                </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-        </div>
-    </div>
-    <?php unset($_SESSION['toast_success']); ?>
-<?php endif; ?>
-
     <div class="row">
     <?php while ($row = mysqli_fetch_assoc($works)): ?>
     <div class="col-md-4 mb-4">
