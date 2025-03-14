734b7c45e5e0904119036bf441d81f6a1019526e
GRIMAGES <redmercy.ros@gmail.com>
2025-03-15 01:19:44 +0800

    update

--- Frontend/admin_dashboard/admin_works.php
+++ Frontend/admin_dashboard/admin_works.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include('../../Backend/connection.php');include('connection.php');  // Ensure the connection is included
+include('../../Backend/connection.php'); // Ensure the connection is included
 
 session_start();
 // Set session timeout duration (in seconds)
