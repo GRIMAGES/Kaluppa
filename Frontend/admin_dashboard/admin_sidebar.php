1057e6a6dbdd7479910db70e3c86005288d978a7
GRIMAGES <redmercy.ros@gmail.com>
2025-03-16 05:32:11 +0800

    update

--- Frontend/admin_dashboard/admin_sidebar.php
+++ Frontend/admin_dashboard/admin_sidebar.php
@@ -52,6 +52,7 @@ $navLinks = [
 
 <link rel="stylesheet" href="../CSS/admin_css/sidebar.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <button class="burger-button d-block d-md-none" onclick="toggleSidebar()">☰</button>
 
