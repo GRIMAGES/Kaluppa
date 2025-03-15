83092874ac920d274579ef271ff507d4485fd59f
GRIMAGES <redmercy.ros@gmail.com>
2025-03-16 03:53:07 +0800

    update

--- Frontend/admin_dashboard/admin_courses.php
+++ Frontend/admin_dashboard/admin_courses.php
@@ -307,7 +307,7 @@ if ($scholarship_result->num_rows > 0) {
     <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="addCourseModalLabel" style="color:black;">Add New Course</h5>
+                <h5 class="modal-title" id="addCourseModalLabel" style="color:white;">Add New Course</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -364,7 +364,7 @@ if ($scholarship_result->num_rows > 0) {
     <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editCourseModalLabel" style="color:black;">Edit Course</h5>
+                <h5 class="modal-title" id="editCourseModalLabel" style="color:white;">Edit Course</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
