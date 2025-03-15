68a186016aeb7b1986fe1fb442c8af3acff0faad
GRIMAGES <redmercy.ros@gmail.com>
2025-03-16 04:20:35 +0800

    update

--- Frontend/admin_dashboard/admin_courses.php
+++ Frontend/admin_dashboard/admin_courses.php
@@ -395,7 +395,7 @@ if ($scholarship_result->num_rows > 0) {
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="approvedUsersModalLabel" style="color:black;">Enrolled Users</h5>
+                <h5 class="modal-title" id="approvedUsersModalLabel" style="color:white;">Enrolled Users</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body" id="approvedUsersModalBody" style="color:black;">
