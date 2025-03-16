d65d6fa6176bd82de0d11b1f19f8fd361af0d124
GRIMAGES <redmercy.ros@gmail.com>
2025-03-16 20:39:33 +0800

    update

--- Backend/admin_controller/generate_certificate.php
+++ Backend/admin_controller/generate_certificate.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once '../connection.php';
 require('../lib/fpdf.php'); // Adjust path based on where you put fpdf.php
 session_start();
