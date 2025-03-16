33cfa3628cb6185f5d6dccecbb2cca46bb44ecc8
GRIMAGES <redmercy.ros@gmail.com>
2025-03-17 04:39:15 +0800

    update

--- Backend/admin_controller/generate_certificate.php
+++ Backend/admin_controller/generate_certificate.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Include the database connection and vendor autoload (if you use any libraries like FPDF or GD)
 require_once '../connection.php';
 require_once '../../vendor/autoload.php';
