bb551e9fb1bd6cce391fb389ce540521bc2ce298
GRIMAGES <redmercy.ros@gmail.com>
2025-03-18 19:44:34 +0800

    update

--- Frontend/admin_dashboard/admin_events.php
+++ Frontend/admin_dashboard/admin_events.php
@@ -131,7 +131,6 @@ if (isset($_GET['delete_event'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/admin_css/event.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
 <body>
