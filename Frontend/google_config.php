d00178c7200271a04654342a45186283e43cdc53
GRIMAGES <redmercy.ros@gmail.com>
2025-03-19 20:23:47 +0800

    update

--- Frontend/google_config.php
+++ Frontend/google_config.php
@@ -3,8 +3,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
  
 
 $client = new Google_Client();
-$client->setClientId('28291478845-l3qaf47fh434u9q8ov5upl1979eprm1b.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('275298191714-6di06o4lcemkk8rhhvlba5griugtpnj6.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('https://kaluppa.online/Kaluppa/Frontend/login_google.php');
 $client->addScope('email');
 $client->addScope('profile');
