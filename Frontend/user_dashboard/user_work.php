960bda9f7d9c8130c6a4b791d9aabc7701829a5f
GRIMAGES <redmercy.ros@gmail.com>
2025-03-19 23:03:26 +0800

    update

--- Frontend/user_dashboard/user_work.php
+++ Frontend/user_dashboard/user_work.php
@@ -60,7 +60,7 @@ if (!$workResult) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Work</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/user_css/user_work.css">
+    <link rel="stylesheet" href="../CSS/user_css/user_courses.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
         .modal-body p {
