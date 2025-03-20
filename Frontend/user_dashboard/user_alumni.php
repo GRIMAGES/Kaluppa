0afce85d8b547af4f19609f2a35d5773b457cc9b
GRIMAGES <redmercy.ros@gmail.com>
2025-03-20 23:05:37 +0800

    update

--- Frontend/user_dashboard/user_alumni.php
+++ Frontend/user_dashboard/user_alumni.php
@@ -31,7 +31,7 @@ if ($result->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/user_css/user_dashboard.css">
+    <link rel="stylesheet" href="../CSS/user_css/user_alumni.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
