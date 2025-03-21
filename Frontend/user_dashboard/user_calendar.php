9e0494e874593ad84f225bd1e229889dfa5eb938
GRIMAGES <redmercy.ros@gmail.com>
2025-03-21 21:55:09 +0800

    update

--- Frontend/user_dashboard/user_calendar.php
+++ Frontend/user_dashboard/user_calendar.php
@@ -59,7 +59,7 @@ if ($coursesResult->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/user_css/.css">
+    <link rel="stylesheet" href="../CSS/user_css/calendars.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body style="background-color: #ddead1;">
