12892a0e5db4d3f20c1fd83006761302138a7f55
GRIMAGES <redmercy.ros@gmail.com>
2025-03-24 21:39:39 +0800

    update

--- Backend/Multiuserlogins.php
+++ Backend/Multiuserlogins.php
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['reg_first_name'], $_P
     ) VALUES (?, ?, ?, ?, ?, 'user', ?, ?, ?, ?, ?, ?, ?, ?, 0)");
     
     $stmt->bind_param(
-        "ssssssssssssss",  // 14 values (excluding hardcoded ones)
+        "sssssssssssss",  // 14 values (excluding hardcoded ones)
         $first_name,
         $middle_name,
         $last_name,
