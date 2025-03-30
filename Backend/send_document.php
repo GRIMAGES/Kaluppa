4f109b7341f9ff58cfc8934791ab683a517b933f
GRIMAGES <redmercy.ros@gmail.com>
2025-03-31 02:56:25 +0800

    update

--- Backend/send_document.php
+++ Backend/send_document.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once  '/connection.php';
+require_once  __DIR__ . '/connection.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
