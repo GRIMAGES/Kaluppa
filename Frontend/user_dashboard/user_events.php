c6b0eda9d5f294c0ea17eba2c7f55729b65f1c54
GRIMAGES <redmercy.ros@gmail.com>
2025-04-05 17:21:07 +0800

    update

--- Frontend/user_dashboard/user_events.php
+++ Frontend/user_dashboard/user_events.php
@@ -32,7 +32,7 @@ if ($result->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Events</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/user_css/user_events.css"> <!-- Ensure this file is included -->
+    <link rel="stylesheet" href="../CSS/user_css/user_event.css"> <!-- Ensure this file is included -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body style="background: #ddead1;">
