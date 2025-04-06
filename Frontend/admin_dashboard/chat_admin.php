0d35fc202ad98032b37fd6c39d50cfc25cfe408c
GRIMAGES <redmercy.ros@gmail.com>
2025-04-06 22:03:31 +0800

    update

--- Frontend/admin_dashboard/chat_admin.php
+++ Frontend/admin_dashboard/chat_admin.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once '../../Backend/connection.php';
 require_once '../../Backend/log_helper.php'; // Include log_helper.php
 
