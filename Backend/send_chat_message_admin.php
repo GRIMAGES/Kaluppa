f10db2cb4a212e9bee0d4b13018c89ee56e563fc
GRIMAGES <redmercy.ros@gmail.com>
2025-04-06 22:07:16 +0800

    update

--- Backend/send_chat_message_admin.php
+++ Backend/send_chat_message_admin.php
@@ -1,8 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'connection.php';
 session_start();
 
-if (!isset($_SESSION['admin_email'])) {
+if (!isset($_SESSION['email'])) {
     header("Location: /Admin/login.php");
     exit();
 }
