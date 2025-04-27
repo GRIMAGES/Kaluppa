2886dccc21f9b8a5391b0ec0e0229e6c20d7b4d8
GRIMAGES <redmercy.ros@gmail.com>
2025-04-28 01:50:04 +0800

    update

--- Backend/user_controller/submit_applications.php
+++ Backend/user_controller/submit_applications.php
@@ -4,8 +4,8 @@ require_once '../../Backend/log_helper.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
-        // Generate a unique ID for the application
-        $applicationId = 'VOL-' . uniqid();
+        // Generate a unique ID for the application (shortened to fit column length)
+        $applicationId = 'VOL-' . substr(uniqid(), -8);
 
         // Collect form data
         $workId = $_POST['work_id'];
